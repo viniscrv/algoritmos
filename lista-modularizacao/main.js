bd5f6a185723ee6e66a945dc46cb6fe8215821c2
Vinícius <viniscrv@gmail.com>
2022-10-11 02:54:50 -0300

    add ex5

--- lista-modularizacao/main.js
+++ lista-modularizacao/main.js
@@ -1,8 +1,8 @@
-/* 1 */
+/* 1.	Elabore uma função que verifica se um número é perfeito ou não. Número perfeito é um número natural cuja soma dos seus divisores próprios (excluído o próprio número) coincide com o número. */
 
 let num = Number(prompt('Digite um número, para verificar se ele é perfeito ou não:'));
 
-function numeroPerfeito(){
+function numeroPerfeito(num){
 
     let divisoresAnteriores = []; 
 
@@ -27,10 +27,9 @@ function numeroPerfeito(){
         
     }
 }
+console.log(numeroPerfeito(num));
 
-console.log(numeroPerfeito());
-
-/* 2 */
+/* 2.	Elabore uma função que efetue a exponenciação de um número x por um expoente y (sem usar a função de exponenciação).*/
 
 let exponenciacao = (x, y) => x**y;
 
@@ -61,3 +60,65 @@ function media(n1,n2,n3){
 }
 
 media(nota1,nota2,nota3);
+
+
+/* 4 */
+
+function colisao(x2, x1, y2, y1, z2, z1){
+    let distancia = Math.sqrt(Math.pow((x2-x1),2) + Math.pow((y2-y1),2) + Math.pow(z2-z1,2));
+
+    if (distancia === 0){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+/* 5 [INCOMPLETO]*/
+
+let exercicio = 1; // prompt
+
+function exercicios(){
+
+    switch(exercicio){
+        case 1:
+                let valor1 = Number(prompt('Informe o *primeiro* valor:'));
+                let valor2 = Number(prompt('Informe o *segundo* valor:'));
+                let valor3 = Number(prompt('Informe o *terceiro* valor:'));
+            const valores = (valor1,valor2,valor3) => {
+                let somaValores = Math.pow(valor1,2);
+                somaValores += Math.pow(valor2,2);
+                somaValores += Math.pow(valor3,2);
+                return somaValores;
+            }
+            console.log(valores(valor1,valor2,valor3));
+            break;
+        case 2:
+
+            let linhas = Number(prompt('Informe quantas linha terá o retângulo:'));
+            let colunas = Number(prompt('Informe quantas colunas terá o retângulo:'));
+
+            const desenharRetangulo = (linhas,colunas) => {
+                let desenho = ''
+                for (c=0; c<colunas; c++){
+                    if (c === 0 /* || c === colunas - 1 */){
+                        desenho += '+'
+                    } else if(c === colunas - 1){
+                        desenho += '+\n'
+                        
+                    }else {
+                        desenho += '-'
+                    }
+                    
+                }
+                console.log(desenho)
+            }
+            desenharRetangulo(3,5)
+
+        case 3:
+        case 4:
+        case 5:
+        default:
+    }
+}
