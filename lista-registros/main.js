6f8b09afaba9ce720789c9a306f26b8bcffe5140
Vinícius <80614001+viniscrv@users.noreply.github.com>
2022-11-18 22:01:45 -0300

    adicionado exercícios em lista-registros

--- lista-registros/main.js
+++ lista-registros/main.js
@@ -88,3 +88,86 @@ funcionariosAbaixo.forEach((funcionario) => {
         Salário do funcionário: ${funcionario.salario}\n
         `)
 });
+
+
+/* Elabore um programa que armazene em uma agenda o nome, o endereço e o telefone de 10 pessoas.
+Após a leitura de todos os dados, leia do teclado um nome a ser buscado nesta agenda. Se o nome for 
+encontrado, apresente todos os dados da pessoa na tela, Senão apresente a mensagem "Nome não encontrado". */
+
+class Registro {
+    constructor (nome, endereco, telefone){
+        this.nome = nome,
+        this.endereco = endereco,
+        this.telefone = telefone
+    }
+}
+let agenda = [];
+let pessoa;
+
+for (let i=0; i<10; i++){
+
+    pessoa = new Registro();
+    pessoa.nome = prompt(`Digite o nome da pessoa [${i+1}] a ser registrada: `);
+    pessoa.endereco = prompt(`Digite o endereço da pessoa [${i+1}] a ser registrada: `);
+    pessoa.telefone = Number(prompt(`Digite o telefone da pessoa [${i+1}] a ser registrada: `));
+
+    agenda.push(pessoa);
+}
+
+const buscarRegistro = () => {
+
+    let nome = prompt("Digite um nome para buscar na agenda: ");
+
+    let encontrados = agenda.filter(registro => registro.nome === nome);
+
+    return encontrados.length >= 1 ? encontrados : "Nome não encontrado";
+}
+
+buscarRegistro();
+
+
+/* Elabore um programa que armazene a matrícula, o nome e o ano de nascimento de 10 pessoas. A seguir:
+1. Apresente a matrícula, o nome e a idade das pessoas cadastradas.
+2. Apresente a idade média das pessoas cadastradas.
+3. Apresente os dados das pessoas cujo ano de nascimento é igual ou superior por um ano informado pelo teclado.
+ */
+
+class Cadastro {
+    constructor (matricula, nome, anoNascimento) {
+        this.matricula = matricula,
+        this.nome = nome,
+        this.anoNascimento = anoNascimento
+    }
+}
+
+let cadastros = [];
+let pessoa;
+
+for (let i=0; i<2; i++) {
+    pessoa = new Cadastro();
+    pessoa.matricula = Number(prompt(`Digite a matricula da pessoa [${i+1}]:`));
+    pessoa.nome = prompt(`Digite o nome da pessoa [${i+1}]:`);
+    pessoa.anoNascimento = Number(prompt(`Digite o ano de nascimento da pessoa [${i+1}]:`));
+
+    cadastros.push(pessoa);
+}
+
+soma = 0;
+
+const data = new Date();
+const anoAtual = data.getFullYear();
+
+for (let i=0; i<cadastros.length; i++) {
+
+    let idade = (anoAtual - cadastros[i].anoNascimento)
+    soma += idade;
+
+    console.log(`
+        Registro [${i+1}]\n 
+        Matrícula : ${cadastros[i].matricula}\n
+        Nome : ${cadastros[i].nome}\n 
+        Idade : ${idade}
+        `)
+}
+
+console.log(`Média de idade entre os cadastros: ${soma/cadastros.length}`);
